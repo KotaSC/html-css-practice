85dd85c558d9e180d919ca0ee6cb64d0b429c492
KotaSC <is0302ke@gmail.com>
2023-02-12 21:42:43 +0900

    Flexbox practice

--- chapter3/src/App.tsx
+++ chapter3/src/App.tsx
@@ -2,24 +2,50 @@ import React from "react";
 import "./App.css";
 import styled from "styled-components";
 
-const Container = styled.section`
-  background-color: #fffeee;
+type FlexContainerProps = {
+  direction?: string;
+};
+
+const Container = styled.div`
+  background-color: #f6f0cc;
+  padding: 20px;
 `;
 
-const Text = styled.h1`
-  color: #0bd;
+const FlexContainer = styled.div<FlexContainerProps>`
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: ${(props) => props.direction || "initial"};
+  border-bottom: solid 5px #432c02;
 `;
 
-const SubText = styled.p`
-  font-size: 20px;
+const Item = styled.div`
+  background-color: #3aacad;
+  color: #fff;
+  margin: 20px;
+  padding: 10px;
 `;
 
 function App() {
   return (
-    <Container>
-      <Text>Text</Text>
-      <SubText>SubText</SubText>
-    </Container>
+    <>
+      <Container>
+        <h1>flex-direction: row</h1>
+        <FlexContainer direction="row">
+          <Item>Item 1</Item>
+          <Item>Item 2</Item>
+          <Item>Item 3</Item>
+          <Item>Item 4</Item>
+        </FlexContainer>
+
+        <h1>flex-direction: row-reverse</h1>
+        <FlexContainer direction="row-reverse">
+          <Item>Item 1</Item>
+          <Item>Item 2</Item>
+          <Item>Item 3</Item>
+          <Item>Item 4</Item>
+        </FlexContainer>
+      </Container>
+    </>
   );
 }
 
