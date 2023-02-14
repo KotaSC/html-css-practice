6f8d959b1ef163005d216e0072b2f58d818bf91e
KotaSC <is0302ke@gmail.com>
2023-02-14 19:23:05 +0900

    align-items study P.156

--- chapter3/src/App.tsx
+++ chapter3/src/App.tsx
@@ -5,6 +5,8 @@ import styled from "styled-components";
 type FlexContainerProps = {
   direction?: string;
   justifyContent?: string;
+  height?: string;
+  alignItems?: string;
 };
 
 const FlexContainer = styled.div<FlexContainerProps>`
@@ -12,6 +14,8 @@ const FlexContainer = styled.div<FlexContainerProps>`
   display: flex;
   flex-direction: ${(props) => props.direction || "initial"};
   justify-content: ${(props) => props.justifyContent || "flex-start"};
+  height: ${(props) => props.height || "auto"};
+  align-items: ${(props) => props.alignItems || "stretch"};
   border-bottom: solid 5px #432c02;
 `;
 
@@ -56,6 +60,29 @@ function App() {
 
         <h1>justify-content: space-around</h1>
         <FlexContainer justifyContent="space-around">{ItemElem}</FlexContainer>
+
+        <h1>align-items: stretch</h1>
+        <FlexContainer height="30vh">{ItemElem}</FlexContainer>
+
+        <h1>align-items: flex-start</h1>
+        <FlexContainer height="30vh" alignItems="flex-start">
+          {ItemElem}
+        </FlexContainer>
+
+        <h1>align-items: flex-end</h1>
+        <FlexContainer height="30vh" alignItems="flex-end">
+          {ItemElem}
+        </FlexContainer>
+
+        <h1>align-items: center</h1>
+        <FlexContainer height="30vh" alignItems="center">
+          {ItemElem}
+        </FlexContainer>
+
+        <h1>align-items: baseline</h1>
+        <FlexContainer height="30vh" alignItems="baseline">
+          {ItemElem}
+        </FlexContainer>
       </Container>
     </>
   );
