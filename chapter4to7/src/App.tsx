6fccacb29b0a193d38b864acebc2e59d85039cf3
KotaSC <is0302ke@gmail.com>
2023-02-18 21:34:03 +0900

    ホーム画面の作成

--- chapter4to7/src/App.tsx
+++ chapter4to7/src/App.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import styled, { css } from "styled-components";
 import { Head } from "./components/Head/Head";
 import { RouterConfig } from "./routes/RouterConfig";
+import { useLocation } from "react-router-dom";
+import homeBgImg from "./assets/images/main-bg.jpg";
+
+type BgPictureProps = {
+  pagePath: String;
+};
+
+const BgPicture = styled.div<BgPictureProps>`
+  background-size: cover;
+  background-position: center top;
+  ${(props) =>
+    props.pagePath === "/" &&
+    css`
+      background-image: url(${homeBgImg});
+    `}
+
+  min-height: 100vh;
+`;
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-      <Head />
-      <RouterConfig />
+      <BgPicture pagePath={location.pathname}>
+        <Head />
+        <RouterConfig />
+      </BgPicture>
     </>
   );
 }
--- chapter4to7/src/components/Home/Home.tsx
+++ chapter4to7/src/components/Home/Home.tsx
@@ -1,5 +1,44 @@
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 4%;
+  text-align: center;
+  margin-top: 10%;
+`;
+
+const PageTitle = styled.h2`
+  font-size: 5rem;
+  font-family: "Philosopher", serif;
+  text-transform: none;
+  font-weight: normal;
+`;
+
+const HomeText = styled.p`
+  font-size: 1.25rem;
+  margin: 10px 0 42px;
+`;
+
+const MenuBtn = styled.a`
+  font-size: 1.375rem;
+  background: #0bd;
+  color: #fff;
+  border-radius: 5px;
+  padding: 18px 32px;
+  &:hover {
+    background: #0090aa;
+  }
+`;
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <PageTitle>We'll Make Your Day</PageTitle>
+      <HomeText>
+        おしゃれなカフェで癒されてみませんか？無添加の食材で体の中からリフレッシュ．
+      </HomeText>
+      <MenuBtn href="/menu">メニューを見る</MenuBtn>
+    </Wrapper>
+  );
 };
